98fad4d0635dd0c86c5d22c5f257bd599e7712b0
Tony <tonypierrekim@gmail.com>
2025-07-17 20:57:23 -0400

    asbad

--- hooks/useBoard.ts
+++ hooks/useBoard.ts
@@ -1,12 +1,12 @@
-import { useQuery } from "react-query";
-import { ColumnModel } from "@/models/board";
+import { useMutation, useQuery } from "react-query";
+import { BoardModel, ColumnModel } from "@/models/board";
 import { useAuth } from "@/store/authStore";
 import { api } from "@/services/api";
 
 export const useBoard = () => {
   const user = useAuth((state) => state.user);
 
-  return useQuery<ColumnModel[], Error>({
+  return useQuery<BoardModel, Error>({
     queryKey: ["board", user?.teams[0]],
     enabled: Boolean(user?.teams?.length),
     queryFn: async () => {
@@ -14,9 +14,46 @@ export const useBoard = () => {
         throw new Error("User not authenticated");
       }
 
-      const response = await api.get<ColumnModel[]>("/board", {
+      const response = await api.get<BoardModel>("/board", {
         params: { team: user.teams[0] },
       });
+      return response.data;
+    },
+  });
+};
+
+export const useCreateBoard = () => {
+  const user = useAuth((state) => state.user);
+
+  return useMutation({
+    mutationKey: ["createBoard"],
+    mutationFn: async () => {
+      if (!user || !user.teams?.length) {
+        throw new Error("User not authenticated or has no team");
+      }
+
+      const response = await api.post("/board", {
+        team: user.teams[0],
+      });
+
+      return response.data;
+    },
+  });
+};
+export const useUpdateBoard = () => {
+  const user = useAuth((state) => state.user);
+
+  return useMutation({
+    mutationKey: ["updateBoard"],
+    mutationFn: async (board: BoardModel) => {
+      if (!user || !user.teams?.length) {
+        throw new Error("User not authenticated or has no team");
+      }
+
+      const response = await api.put("/board", {
+        board: board,
+      });
+
       return response.data;
     },
   });
