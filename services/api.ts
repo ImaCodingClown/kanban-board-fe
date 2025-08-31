0d78570e03a90845e0593056bef92301aedceab7
YoungSuChae <codudtn369@gmail.com>
2025-08-31 03:21:31 -0400

    baseURL fixed

--- services/api.ts
+++ services/api.ts
@@ -9,10 +9,10 @@ declare module "axios" {
 }
 
 export const api = axios.create({
-  //baseURL: "https://kanban-board-be.onrender.com", // your Rust backend
+  baseURL: "https://kanban-board-be.onrender.com", // your Rust backend
 
-  baseURL: "http://localhost:8080", // for local development
-  timeout: 10000,
+  // baseURL: "http://localhost:8080", // for local development
+  timeout: 5000,
 });
 
 const RETRY_CONFIG = {
